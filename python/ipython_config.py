6aad741b39e460c7907985e0de150f8a08aae8b4
williamstravis <travis.williams@nrel.gov>
2023-08-17 20:18:16 -0600

    Updated ipython startup script.

--- python/ipython_config.py
+++ python/ipython_config.py
@@ -6,50 +6,50 @@ Created on Wed Jun 10 16:00:02 2020
 
 @author: twillia2
 """
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    pass
+
+
 
 def startup():
     """Create magic aliases and bookmarks from bash aliases."""
     import os
 
-    from IPython import get_ipython 
+    from IPython import get_ipython
+
+    # Indicate that it's being run
+    print(f"Running startup script: {__file__}")
+
+    # Use simple pager
+    os.environ["PAGER"] = "cat"
 
     # Create interactive shell and alias manager objects
     ip = get_ipython()
     manager = ip.alias_manager
 
     # Read in bash alias file
-    if os.path.exists(os.path.expanduser("~/.bash_aliases")):
-        alias_path = os.path.expanduser("~/.bash_aliases")
-    elif os.path.exists(os.path.expanduser("~/.bashrc")):
-        alias_path = os.path.expanduser("~/.bashrc")
-    else:
-        alias_path = os.path.expanduser("~/.bash_profile")
+    alias_path = os.path.expanduser("~/.bash_aliases")
     lines = open(alias_path).readlines()
 
     # Check each line in the file for an alias and get its command
     for line in lines:
         if line.startswith("alias"):
-            line = ' '.join(line.split()[1:])
-            split = line.find('=')
+            line = " ".join(line.split()[1:])
+            split = line.find("=")
             alias = line[:split]
             cmd = line[split + 1:][1:-1]
 
             # Use alias magic if the command does not change directories only
-            if "cd " not in cmd or len(cmd.split()) > 2:
+            if not cmd.startswith("cd ") or len(cmd.split()) > 2:
                 manager.soft_define_alias(alias, cmd)
 
             # Otherwise, use bookmarks
             else:
-                cmd = cmd.replace("cd ", "")
-                ip.run_line_magic("bookmark {} {}".format(alias, cmd))
+                loc = cmd.replace("cd ", "")
+                ip.run_line_magic("bookmark", f"{alias} {loc}")
 
-
-def magic_editor():
-    """Use magic to open file in editor."""
-    from IPython import TerminalInteractiveShell
-
-    TerminalInteractiveShell.editor
-                
 
 if __name__ == "__main__":
     startup()
